33048f6a84bf8ca25ee9d0543d70b4df8edb9222
davidrose23 <darose9923@gmail.com>
2023-02-13 14:28:20 -0500

    fixed favicon error

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.get('/favicon.ico', (req, res) => {
+    res.status(204).end();
+  });
+  
 
 app.get('/', (req, res) => {
     res.render('landing');
